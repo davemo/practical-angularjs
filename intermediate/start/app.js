56c4a37598bb3da36764891fe22938a54b7111dc
David Mosher <davidmosher@gmail.com>
2014-11-20 08:11:26 -0500

    migrate app.js from fundamentals/finish to intermediate/start

--- intermediate/start/app.js
+++ intermediate/start/app.js
@@ -1,161 +1,50 @@
-var app = angular.module("app", ['ngSanitize', 'ngRoute']);
+var app = angular.module("app", ['ngRoute'])
 
-app.config(function($httpProvider) {
-
-  var logsOutUserOn401 = function($location, $q, SessionService, FlashService) {
-    return {
-      responseError: function(rejection) {
-        if(rejection.status === 401) {
-          SessionService.unset('authenticated');
-          $location.path('/login');
-          FlashService.show(rejection.data.flash);
-          return $q.reject(rejection);
-        } else {
-          return rejection;
-        }
-      }
-    };
-  };
-
-  $httpProvider.interceptors.push(logsOutUserOn401);
-
-});
-
-app.config(function($routeProvider, $locationProvider) {
-
-  $locationProvider.html5Mode(true);
+app.config(function($routeProvider) {
 
   $routeProvider.when('/login', {
-    templateUrl: 'templates/login.html',
+    templateUrl: 'login.html',
     controller: 'LoginController'
   });
 
   $routeProvider.when('/home', {
-    templateUrl: 'templates/home.html',
+    templateUrl: 'home.html',
     controller: 'HomeController'
-  });
-
-  $routeProvider.when('/list-of-books', {
-    templateUrl: 'templates/books.html',
-    controller: 'BooksController',
-    resolve: {
-      books : function(BookService) {
-        return BookService.get();
-      }
-    }
   });
 
   $routeProvider.otherwise({ redirectTo: '/login' });
 
 });
 
-app.run(function($rootScope, $location, AuthenticationService, FlashService) {
-  var routesThatRequireAuth = ['/home'];
-
-  $rootScope.$on('$routeChangeStart', function(event, next, current) {
-    if(_(routesThatRequireAuth).contains($location.path()) && !AuthenticationService.isLoggedIn()) {
-      $location.path('/login');
-      FlashService.show("Please log in to continue.");
-    }
-  });
-});
-
-app.factory("BookService", function($http) {
-  return {
-    get: function() {
-      return $http.get('/books');
-    }
-  };
-});
-
-app.factory("FlashService", function($rootScope) {
-  return {
-    show: function(message) {
-      $rootScope.flash = message;
-    },
-    clear: function() {
-      $rootScope.flash = "";
-    }
-  };
-});
-
-app.factory("SessionService", function() {
-  return {
-    get: function(key) {
-      return sessionStorage.getItem(key);
-    },
-    set: function(key, val) {
-      return sessionStorage.setItem(key, val);
-    },
-    unset: function(key) {
-      return sessionStorage.removeItem(key);
-    }
-  };
-});
-
-app.factory("AuthenticationService", function($http, $sanitize, SessionService, FlashService, CSRF_TOKEN) {
-
-  var cacheSession   = function() {
-    SessionService.set('authenticated', true);
-  };
-
-  var uncacheSession = function() {
-    SessionService.unset('authenticated');
-  };
-
-  var loginError = function(response) {
-    FlashService.show(response.flash);
-  };
-
-  var sanitizeCredentials = function(credentials) {
-    return {
-      email: $sanitize(credentials.email),
-      password: $sanitize(credentials.password),
-      csrf_token: CSRF_TOKEN
-    };
-  };
-
+app.factory("AuthenticationService", function($location) {
   return {
     login: function(credentials) {
-      var login = $http.post("/auth/login", sanitizeCredentials(credentials));
-      login.success(cacheSession);
-      login.success(FlashService.clear);
-      login.error(loginError);
-      return login;
+      if (credentials.username !== "ralph" || credentials.password !== "wiggum") {
+        alert("Username must be 'ralph', password must be 'wiggum'");
+      } else {
+        $location.path('/home');
+      }
     },
     logout: function() {
-      var logout = $http.get("/auth/logout");
-      logout.success(uncacheSession);
-      return logout;
-    },
-    isLoggedIn: function() {
-      return SessionService.get('authenticated');
+      $location.path('/login');
     }
   };
 });
 
 app.controller("LoginController", function($scope, $location, AuthenticationService) {
-  $scope.credentials = { email: "", password: "" };
+  $scope.credentials = { username: "", password: "" };
 
   $scope.login = function() {
-    AuthenticationService.login($scope.credentials).success(function() {
-      $location.path('/home');
-    });
-  };
+    AuthenticationService.login($scope.credentials);
+  }
 });
 
-app.controller("BooksController", function($scope, books) {
-  $scope.books = books.data;
-});
-
-app.controller("HomeController", function($scope, $location, AuthenticationService) {
+app.controller("HomeController", function($scope, AuthenticationService) {
   $scope.title = "Awesome Home";
   $scope.message = "Mouse Over these images to see a directive at work!";
 
   $scope.logout = function() {
-    AuthenticationService.logout().success(function() {
-      $location.path('/login');
-    });
+    AuthenticationService.logout();
   };
 });
 
