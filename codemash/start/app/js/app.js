7d427dda86a9de7fec3140bb6ee4f69a28835bbe
David Mosher <davidmosher@gmail.com>
2015-01-01 14:22:56 -0500

    end with ui-router, ui-sref, ng-include

--- codemash/start/app/js/app.js
+++ codemash/start/app/js/app.js
@@ -1,2 +1,19 @@
 // setter
-angular.module("app", []); // dependencies go in the array as strings, which are names of modules.
+var app = angular.module("app", ["ui.router"]); // dependencies go in the array as strings, which are names of modules.
+
+app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $locationProvider.html5Mode(true); // for pushState routing support instead of # hash
+
+  $stateProvider.state('cards', {
+    url: "/cards",
+    templateUrl: "cards.html"
+  });
+
+  // default route
+
+  $urlRouterProvider.otherwise('/cards');
+
+  // admin, decks
+
+});
