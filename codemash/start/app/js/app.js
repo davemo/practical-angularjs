bf183d5f8ed0d74b1acd3b9870c6459254afb005
David Mosher <davidmosher@gmail.com>
2015-01-01 21:05:36 -0500

    end of exercise 4 - includes pagination, filtering, animation, detailview (refactored)

--- codemash/start/app/js/app.js
+++ codemash/start/app/js/app.js
@@ -1,5 +1,5 @@
 // setter
-var app = angular.module("app", ["ui.router"]); // dependencies go in the array as strings, which are names of modules.
+var app = angular.module("app", ["ui.router", "ngAnimate"]); // dependencies go in the array as strings, which are names of modules.
 
 app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
@@ -8,7 +8,21 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
   $stateProvider.state('cards', {
     url: "/cards",
     templateUrl: "cards.html",
-    controller: "CardsController"
+    controller: "CardsController",
+    resolve: {
+      cardsResponse: function($http) {
+        return $http.get("/api/cards");
+      }
+    }
+  });
+
+  $stateProvider.state('cards.detail', { // nested state, url segments built from parent + children
+    url: '/:id',
+    templateUrl: "detail.html",
+    controller: "DetailViewController",
+    onEnter: function($stateParams) {
+      console.log($stateParams.id);
+    }
   });
 
   // default route
@@ -19,13 +33,105 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
 });
 
-app.controller("CardsController", function($scope, $http) {
+app.run(function($rootScope, $state) {
 
-  // fetch the cards from our API
-  $http.get("/api/cards").success(function(response) {
-    // attach them to the $scope
-    $scope.cards = response.cards;
+  $rootScope.searchQueryChanged = function(query) {
+    $rootScope.searchQuery = query;
+  };
+
+  $rootScope.$state = $state;
+
+});
+
+app.controller("DetailViewController", function($scope, $state, $stateParams, cardsResponse) {
+
+  var cards = cardsResponse.data.cards; // child states can inject parent resolved dependencies
+
+  // we need to detect a state change and extract the right card to show in the case
+  // where the user comes to the /cards/:id url from a full page reload
+
+  $scope.$on('$stateChangeSuccess', function() {
+    $scope.card = _(cards).findWhere({id: parseInt($stateParams.id,10)});
   });
 
-  // iterate over them in the template to render images
+  $scope.isDetailState = function() {
+    return $state.is('cards.detail');
+  };
+
+  $scope.hide = function() {
+    $state.go('cards');
+  };
+
 });
+
+app.controller("CardsController", function($scope, $rootScope, $state, cardsResponse, CardFilter) {
+
+  var cards = cardsResponse.data.cards;
+
+  $scope.heroFilters = CardFilter.heroFilters;
+  $scope.manaFilters = CardFilter.manaFilters;
+  $scope.currentHeroFilter = 'neutral';
+  $scope.currentManaFilter = 'all';
+  $scope.currentPage = 0;
+  $scope.totalPages  = 0;
+
+  function renderFilteredCards() {
+    console.log('renderFilteredCards', $scope.currentManaFilter, $scope.currentHeroFilter);
+    var pages = CardFilter.filterCards(cards, $scope.currentManaFilter, $scope.currentHeroFilter);
+    $scope.totalPages = pages.length;
+    $scope.cards = pages[$scope.currentPage];
+  }
+
+  $scope.detailViewFor = function(card) {
+    $state.go('cards.detail', {id: card.id});
+  };
+
+  $scope.prevPage = function() {
+    $scope.currentPage -= 1;
+  };
+
+  $scope.nextPage = function() {
+    $scope.currentPage += 1;
+  };
+
+  $scope.canGoPrev = function() {
+    return $scope.currentPage - 1 >= 0;
+  };
+
+  $scope.canGoNext = function() {
+    return $scope.currentPage < $scope.totalPages - 1;
+  };
+
+  $scope.$watchGroup(['currentManaFilter', 'currentHeroFilter', 'currentPage'], renderFilteredCards);
+
+  $scope.setHeroFilter = function(value) {
+    $scope.currentHeroFilter = value;
+  };
+
+  $scope.setManaFilter = function(value) {
+    $scope.currentManaFilter = value;
+  };
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
